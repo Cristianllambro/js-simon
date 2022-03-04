774fdb7e451ae347040da162afbc612f44d33d14
cristianllambro <llambro.cristian1996@gmail.comm>
2022-03-04 17:52:46 +0100

    i don't know

--- script.js
+++ script.js
@@ -1,28 +1,29 @@
 const result = document.getElementById('result');
 const bntGenerate = document.getElementById('btn-generate');
-let time = 5;
-let gap = 1000;
+let timeSeconds = 30000;
+const arrRandom = [];
 
-const myTime = setInterval(decrementTime, gap)
-
-
+// click button for generate number
 bntGenerate.addEventListener ('click', function () {
-    const arrRandom = [];
+    createTime();
+    result.innerHTML = arrRandom;
+})
+
+
+// function five random number
+function fiveNumberRandom () {
     for (let i = 0; i < 5; i++) {
         let numberRandom = Math.floor(Math.random() * 10 + 1);
         arrRandom.push(numberRandom);
-    
+        
         while (arrRandom.includes(numberRandom)) {
             numberRandom = Math.floor(Math.random() * 10 + 1);
         }
-        result.innerHTML = arrRandom;
     }
-})
+}
 
-function decrementTime () {
-    if (time == 0) {
-        clearInterval(myTime);
-    } else{
-        time--;
-    }
+
+// generate time 30 seconds
+function createTime () {
+    setTimeout(timeSeconds);
 }
