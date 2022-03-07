25ec1d8949c543d316b0f188a6ce70f0ec9e1fe3
cristianllambro <llambro.cristian1996@gmail.comm>
2022-03-07 18:19:48 +0100

    finish

--- script.js
+++ script.js
@@ -1,29 +1,70 @@
 const result = document.getElementById('result');
 const bntGenerate = document.getElementById('btn-generate');
-let timeSeconds = 30000;
-const arrRandom = [];
+const error = document.getElementById('error');
+
+let arrRandom = [];
+let arrCorrect = [];
 
 // click button for generate number
-bntGenerate.addEventListener ('click', function () {
-    createTime();
-    result.innerHTML = arrRandom;
-})
+bntGenerate.addEventListener ('click', function (){
+    if(arrRandom.length != 0) {
+        arrRandom = [];
+        arrCorrect = [];
+    }
+    fiveNumberRandom(10);
+});
 
 
 // function five random number
-function fiveNumberRandom () {
+function fiveNumberRandom (max) {
     for (let i = 0; i < 5; i++) {
-        let numberRandom = Math.floor(Math.random() * 10 + 1);
-        arrRandom.push(numberRandom);
+        let check = false;
         
-        while (arrRandom.includes(numberRandom)) {
-            numberRandom = Math.floor(Math.random() * 10 + 1);
+        while (!check) {
+            let numberRandom = Math.floor(Math.random() * max + 1);
+            let checkValidate = true;
+            for (let j = 0; j < arrRandom.length; j++) {
+                if (numberRandom == arrRandom[j]) {
+                    checkValidate = false;
+                }
+            }
+            
+            if (checkValidate) {
+                arrRandom.push(numberRandom);
+                check = true;
+            }
         }
+        console.log(arrRandom)
     }
+    result.innerHTML = arrRandom;
+    createTime();
 }
 
 
 // generate time 30 seconds
 function createTime () {
-    setTimeout(timeSeconds);
+    setTimeout(function () {
+        result.innerHTML = '';
+        setTimeout(function () {
+            for (let i = 1; i < 6; i++) {
+                let userNumber = parseInt(prompt("Inserisci il numero " + i + ':'));
+                let check = false;
+                for (let j = 0; j < arrRandom.length; j++) {
+                    if (userNumber == arrRandom[j]) {
+                        arrCorrect.push(userNumber);
+                        check = true;
+                    }
+                }
+                if (check){
+                    let index = arrRandom.indexOf(userNumber);
+                    arrRandom.splice(index, 1);
+                }
+            }
+            result.innerHTML = 'numeri giusti: ' + arrCorrect;
+            error.innerHTML = 'numeri sbagliati: ' + arrRandom;
+
+        }, 500);
+    }, 5000);
 }
+
+// l'ho fatto con un aiuto esterno perche' non sriuscivo e avevo un blocco
